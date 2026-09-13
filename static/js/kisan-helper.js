716af357fc4f2880e790b46dc4a7aa4676c12edf
Code_Master_Puspendu <puspendugorai74@gmail.com>
2026-09-13 20:45:02 +0530

    Update kisan-helper.js

--- static/js/kisan-helper.js
+++ static/js/kisan-helper.js
@@ -513,7 +513,7 @@ body.light-theme .kw-speak-btn  { border-color: rgba(22,101,52,.25); color: rgba
     width: 52px; height: 52px;
   }
   #kisanPromptBox {
-    bottom: calc(32px + env(safe-area-inset-bottom, 0px));
+    bottom: calc(90px + env(safe-area-inset-bottom, 0px));
     right: calc(75px + env(safe-area-inset-right, 0px));
   }
   .kisan-prompt-arrow {
